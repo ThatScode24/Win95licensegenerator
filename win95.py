d364da23a2dcea7c6a7f5d9372ed110900230b0e
ThatScode24 <65173747+ThatScode24@users.noreply.github.com>
2020-07-13 17:02:28 +0300

    Added OEM license generator

--- win95.py
+++ win95.py
@@ -4,16 +4,16 @@ import random
 
 def trial():
 
-		d1 = random.randint(1, 9)
-		d2 = random.randint(1, 9)
-		d3 = random.randint(1, 9)
-		d4 = random.randint(1, 9)
-		d5 = random.randint(1, 9)
-		d6 = random.randint(1, 9)
-		d7 = random.randint(1, 9)
-		d8 = random.randint(1, 9)
-		d9 = random.randint(1, 9)
-		d10 = random.randint(1, 9)
+		d1 = random.randint(0, 9)
+		d2 = random.randint(0, 9)
+		d3 = random.randint(0, 9)
+		d4 = random.randint(0, 9)
+		d5 = random.randint(0, 9)
+		d6 = random.randint(0, 9)
+		d7 = random.randint(0, 9)
+		d8 = random.randint(0, 9)
+		d9 = random.randint(0, 9)
+		d10 = random.randint(0, 9)
 		z = int(str(d4) + str(d5) + str(d6) + str(d7) + str(d8) + str(d9) + str(d10))
 
 	
---
+++ win95OEM.py
@@ -0,0 +1,45 @@
+__author__ = "ThatScode"
+
+import random
+
+def trial():
+	d1 = random.randint(100, 366)
+	d2 = random.randint(0, 366)
+	d3 = random.randint(0, 366)
+	d4 = random.randint(95, 99)
+	d5 = 0
+	d6 = random.randint(0, 9)
+	d7 = random.randint(0, 9)
+	d8 = random.randint(0, 9)
+	d9 = random.randint(0, 9)
+	d10 = random.randint(0, 9)
+	d11 = random.randint(0, 9)
+	d12 = random.randint(0, 9)
+	d13 = random.randint(0, 9)
+	d14 = random.randint(0, 9)
+	d15 = random.randint(0, 9)
+	d16 = random.randint(0, 9)
+	
+	z = int(str(d5) + str(d6) + str(d7) + str(d8) + str(d9) + str(d10) + str(d11))
+	
+
+
+	if int(z) % 7 == 0:
+		if not d16 == 0 and not d16 == 8 and not d16 == 9:
+			print(f"{d1}{d4}-OEM-{d5}{d6}{d7}{d8}{d9}{d10}{d11}-{d12}{d13}{d14}{d15}{d16}")
+
+		else:
+			a = 1
+
+	else:
+		a = 1
+
+numtrials = int(input(f"Program made by {__author__}. How many trials do you want? : "))
+
+for i in range(numtrials):
+	trial()
+
+
+
+
+
