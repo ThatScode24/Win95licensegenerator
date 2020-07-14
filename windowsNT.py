d1e50d27b538d7ec129816d29b832947d60ee748
ThatScode24 <65173747+ThatScode24@users.noreply.github.com>
2020-07-14 11:21:34 +0300

    Added windows NT license generator

---
+++ windowsNT.py
@@ -0,0 +1,39 @@
+__author__ = 'miha_focsa'
+
+import random
+
+def trial():
+	d1 = random.randint(100, 999)
+	d2 = random.randint(0, 9)
+	d3 = random.randint(0, 9)
+	d4 = random.randint(0, 9)
+	d5 = random.randint(0, 9)
+	d6 = random.randint(0, 9)
+	d7 = random.randint(0, 9)
+	d8 = random.randint(0, 9)
+
+
+	z = int(str(d2) + str(d3) + str(d4) + str(d5) + str(d6) + str(d7) + str(d8))
+	x = int(d1)
+
+	if int(z) % 7 == 0:
+		
+		if not z == 999 and not z == 888 and not z == 777 and not z == 666 and not z == 555 and not z == 444 and not z == 333:
+			
+			if not d8 == 8 and not d8 == 9 and not d8 == 0:
+				print(f"{d1}-{d2}{d3}{d4}{d5}{d6}{d7}{d8}")
+
+			else:
+				a = 1
+
+		else:
+			a = 1
+	else:
+		a = 1
+
+
+numtrials = int(input(f"Program made by {__author__}. How many trials do you want? : "))
+
+for i in range(numtrials):
+	trial()
+				
